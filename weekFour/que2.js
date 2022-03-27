b24f9f290fe776249fe933ece55b8a55f731c524
Aman Singh <reachout.amansingh@gmail.com>
2022-03-27 18:07:55 +0530

    add weekFour que 2

---
+++ weekFour/que2.js
@@ -0,0 +1,6 @@
+// Take an array with 5 colors. Create another array by adding two more colors to it.
+
+const colors = ['red', 'blue', 'green', 'yellow', 'grey'];
+const newColors = [...colors, 'black', 'white'];
+
+console.log(newColors);
