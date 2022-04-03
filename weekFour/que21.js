c560ff416edb76fe999df82901fd457bb14db4b8
Aman Singh <reachout.amansingh@gmail.com>
2022-04-03 21:19:54 +0530

    add weekFour que21

---
+++ weekFour/que21.js
@@ -0,0 +1,15 @@
+/* Given an array. Write a function that takes in the given array and prints only the numbers which are less than 8 and also an even number.
+
+const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+
+Your output should be: [2, 6, 8, 4]
+
+*/
+
+const arr = [1, 2, 3, 58, 5, 6, 24, 8, 15, 4];
+
+const lessThanEight = num => num < 8 && num % 2 === 0;
+
+const result = arr.filter(lessThanEight);
+
+console.log(result);
