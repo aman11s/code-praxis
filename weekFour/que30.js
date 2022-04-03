a8e278fa9746a92d7f7d549d974d3a5ecf860a9a
Aman Singh <reachout.amansingh@gmail.com>
2022-04-04 01:13:34 +0530

    add weekFour que31 solution

---
+++ weekFour/que30.js
@@ -0,0 +1,6 @@
+/*Write a function that accepts a number as input and inserts hyphens between every two even numbers.
+
+If your input is: 24345687
+
+Your output should be: 2-23456-87
+*/
---
+++ weekFour/que31.js
@@ -0,0 +1,14 @@
+/*
+Write a function that takes in a string and converts all the characters to uppercase. (Hint: toUpperCase()).
+
+If your input is: "neogrammer"
+Your output should be: "NEOGRAMMER"
+
+If your input is: "neoG"
+Your output should be: "NEOG"
+
+*/
+
+const getUpperCase = str => str.toUpperCase();
+const result = (getUpperCase('aman'));
+console.log(result);
